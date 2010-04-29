523d27a63a798b3c41cfb604efc2b696f6688220
Matthew Brett <matthew.brett@gmail.com>
2010-04-29 03:38:56 -0700

    More meaty custom-env example

--- custom-env-eg.el
+++ custom-env-eg.el
@@ -1,4 +1,14 @@
 ;; http://www.emacswiki.org/emacs/ExecPath
 ;; http://xahlee.org/emacs/emacs_env_var_paths.html
-(setenv "PYTHONPATH" (concat (getenv "HOME") "/sp"))
-(setq exec-path (append '("~/usr/local/bin") exec-path))
+(setq py-version-no "2.6")
+(setenv "PYTHONPATH"
+        (concat (getenv "HOME")
+                "/usr/local/lib/python"
+                py-version-no
+                "/site-packages"))
+;; Where python is
+(setq py-exec-path (concat
+                   "/Library/Frameworks/Python.framework/Versions/"
+                   py-version-no
+                   "/bin"))
+(setq exec-path (cons py-exec-path exec-path))
